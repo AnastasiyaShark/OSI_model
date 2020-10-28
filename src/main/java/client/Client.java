b95e69b5b22f3dcd168e3c53279be12dee572c69
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2020-10-28 14:10:45 +0100

    Task 2 "Клиент-сервер с рюшечками"

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,7 +1,9 @@
 package client;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -15,11 +17,34 @@ public class Client {
         this.port = port;
     }
 
-    public void sendMessage (String s){
+    public void sendMessage (String fromName){
         try (Socket clientSocket = new Socket(ip, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         ){
-            out.println(s);
+           String question = in.readLine();
+            System.out.println(question);
+           if (question.equals("Write your name")){
+               out.println(fromName);
+               question = in.readLine();
+               System.out.println(question);
+
+               if (question.equals("Are you child? (yes/no)")){
+                   out.println("yes");
+                   String answer = in.readLine();
+                   System.out.println(answer);
+                   question= in.readLine();
+                   System.out.println(question);
+                   if (question.equals("Are you playing on PC? (yes/no)")){
+                       out.println("yes");
+                       answer = in.readLine();
+                       System.out.println(answer);
+                   }
+               }
+           }
+
+
+
         }  catch (IOException ioException){
             System.out.println(ioException.toString());
         }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -9,7 +9,7 @@ import java.net.Socket;
 
 public class Server {
 
-   private short port;
+    private short port;
 
     public Server(short port) {
         this.port = port;
@@ -19,17 +19,44 @@ public class Server {
         return port;
     }
 
-    public void start (){
+    public void start() {
         try {
             ServerSocket serverSocket = new ServerSocket(port);
-            while (true){
+            while (true) {
                 Socket clientSocket = serverSocket.accept();
-                //PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-                try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-                    String s;
-                    while ((s = in.readLine()) != null) {
-                        System.out.printf("Msg: %s, Port: %d%n", s, clientSocket.getPort());
+
+                try (
+                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
+                ) {
+
+                    out.println("Write your name");
+                    String name = in.readLine();
+                    System.out.println(name);
+                    out.println("Are you child? (yes/no)");
+                    String isChild = in.readLine();
+                    System.out.println(isChild);
+                    if (isChild.equals("yes")) {
+                        out.printf("Welcome to the kids area, %s! Let's play!%n", name);
+                    } else {
+                        if (isChild.equals("no")) {
+                            out.printf("Welcome to the adult zone, %s! Have a good rest, or a good working day!%n", name);
+                        } else {
+                            out.println(" Wrong answer!");
+                        }
+                    }
+                    out.println("Are you playing on PC? (yes/no)");
+                    String onPC = in.readLine();
+                    System.out.println(onPC);
+                    if (onPC.equals("yes")) {
+                        out.printf("Please, visit netology.ru%n");
+                    } else {
+                        if (onPC.equals("no")) {
+                            out.println("Go play on PC");
+                        } else {
+                            out.println(" Wrong answer!");
+                        }
                     }
                     clientSocket.close();
                 } catch (IOException ex) {
@@ -37,8 +64,8 @@ public class Server {
                 }
             }
 
-        } catch (IOException ioException){
-            System.out.println(" Error " + ioException );
+        } catch (IOException ioException) {
+            System.out.println(" Error " + ioException);
         }
 
     }
